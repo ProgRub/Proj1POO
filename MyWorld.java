ee1be33e2d0e471285257c49c708bd90a14cba08
ProgRub <ruben2000jgr@gmail.com>
2020-02-19 16:52:01 +0000

    NadaDemais

--- MyWorld.java
+++ MyWorld.java
@@ -15,7 +15,11 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        // Create a new world with 1300x700 cells with a cell size of 1x1 pixels.
+        super(1300,700, 1); 
+        GreenfootImage bg = new GreenfootImage("Earth.png");
+        bg.scale(1300, 700);
+        setBackground(bg);
+        
     }
 }
