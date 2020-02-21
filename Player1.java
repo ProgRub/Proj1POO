33681775e21cd977932632c3502fc8647a7fa40f
Diego Briceño <diegobriceno13@gmail.com>
2020-02-21 10:55:52 +0000

    player

--- Player1.java
+++ Player1.java
@@ -12,15 +12,23 @@ public class Player1 extends Players
     private String left = "a";
     private String right = "d";
     private String down = "s";
+    private GreenfootImage image1,image2,image3,image4,image5,image6;
+    int contador=0;
     
-    public Player1(){}
-    /**
-     * Act - do whatever the Player1 wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public Player1(){
+        image1 = new GreenfootImage("1.png");
+        image2 = new GreenfootImage("2.png");
+        image3 = new GreenfootImage("3.png");
+        image4 = new GreenfootImage("4.png");
+        image5 = new GreenfootImage("5.png");
+        image6 = new GreenfootImage("6.png");
+        setImage(image1);
+    }
+    
     public void act() 
     {
         move();
+        
     }    
     
     public void move(){
@@ -29,18 +37,53 @@ public class Player1 extends Players
         }
         else if (Greenfoot.isKeyDown(left)){
             setLocation(getX()-2, getY());
+            image1.mirrorHorizontally();
+            animarMove();
+            image2.mirrorHorizontally();
+            image3.mirrorHorizontally();
+            image4.mirrorHorizontally();
+            image5.mirrorHorizontally();
+            image6.mirrorHorizontally();
         }
         else if (Greenfoot.isKeyDown(right)){
             setLocation(getX()+2, getY());
+            animarMove();
         }
         else if (Greenfoot.isKeyDown(down)){
             setLocation(getX(), getY()+2);
         }
     }
     
+<<<<<<< Updated upstream
     public void jump(){
         for (int i=10;i>0;i--){            
         setLocation(getX(),getY()-i);
     }
     }
+=======
+    public void animarMove(){
+        contador++;
+        if (contador==4){
+            if(getImage()==image1){
+                setImage(image2);
+            }
+            else if (getImage()==image2){
+                setImage(image3);
+            }
+             else if(getImage()==image3){
+                setImage(image4);
+            }
+            else if (getImage()==image4){
+                setImage(image5);
+            }
+            else if(getImage()==image5){
+                setImage(image6);
+            }
+            else if (getImage()==image6){
+                setImage(image1);
+        }
+        contador=0;
+    }
+}
+>>>>>>> Stashed changes
 }
