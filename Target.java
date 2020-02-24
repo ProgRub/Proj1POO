5e16d85e9b4d2c717158ec2872aa84a58b991f39
Diego Briceño <diegobriceno13@gmail.com>
2020-02-24 22:07:31 +0000

    target brilha

--- Target.java
+++ Target.java
@@ -8,12 +8,37 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Target extends Actor
 {
-    
+    private int count;
+    private final int TIMER;
+    private int count2;
+    GreenfootImage semBrilho,comBrilho;
     public Target(){
-        setImage(new GreenfootImage("target7.png"));
+        semBrilho = new GreenfootImage("targetf.png");
+        comBrilho = new GreenfootImage("target8.png");
+        setImage(semBrilho);
+        TIMER=5;
+        count = TIMER*4;
+        count2 = 0;
     }
     
     public void act() 
     {
+        switchImage();
+    }
+    
+    public void switchImage()
+    {
+        if (count2%TIMER==0){
+            if (getImage() == semBrilho)
+            {
+                setImage(comBrilho);
+            }
+
+            else if (getImage() == comBrilho)
+            {
+                setImage(semBrilho);
+            }
+        }  
+        count2++;     
     }
 }
