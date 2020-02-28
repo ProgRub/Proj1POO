f3e0d7916d9d0f9136e65ab5df0d411f3fe1b99e
Diego Briceño <diegobriceno13@gmail.com>
2020-02-28 20:12:55 +0000

    pinguim caindo

--- Pinguim.java
+++ Pinguim.java
@@ -9,14 +9,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Pinguim extends Jogos
 {
     private static int contador;
-    GreenfootImage pinguim1,pinguim2,pinguim3,pinguim4,pinguim5,pinguim6;
-    
+    GreenfootImage pinguim1,pinguim2,pinguim3,pinguimCaindo;
+    private boolean caindo =false;
     public Pinguim(){
         pinguim1 = new GreenfootImage("pinguim11.png");
         pinguim2 = new GreenfootImage("pinguim12.png");
         pinguim3 = new GreenfootImage("pinguim13.png");
+        pinguimCaindo = new GreenfootImage("pinguimCaindo.png");
         setImage(pinguim1);
         contador=0;
+        Greenfoot.playSound("penguin.mp3");
+        caindo = false;
     }
     public void act() 
     {
@@ -24,28 +27,44 @@ public class Pinguim extends Jogos
        animar();
        emCimaPlataforma();
        desaparece();
+       caindo();
     } 
     
     public void animar(){
-        contador++;
-        if (contador%10==0){
-        if(getImage() == pinguim1){
-            setImage(pinguim2);
-        }
-        else if (getImage() == pinguim2){
-            setImage(pinguim3);
-        }
-        else if (getImage() == pinguim3){
-            setImage(pinguim2);
-        }
+        if(caindo == false){
+            contador++;
+            if (contador%10==0){
+            if(getImage() == pinguim1){
+                setImage(pinguim2);
+            }
+            else if (getImage() == pinguim2){
+                setImage(pinguim3);
+            }
+            else if (getImage() == pinguim3){
+                setImage(pinguim2);
+            }else if (getImage() == pinguimCaindo){
+                setRotation(0);
+                setImage(pinguim1);
+            }
         contador=0;
+    }
     }
 }
     
     public void emCimaPlataforma(){
-        if (! isTouching(PlataformaGelo.class)){
-            setLocation(getX(), getY() +3);
+        if (!isTouching(PlataformaGelo.class)){
+            setLocation(getX(), getY() +4);
+            caindo=true;
+        }else if (isTouching(PlataformaGelo.class) && caindo){
+            caindo=false;
         }
+    }
+    
+    public void caindo(){
+        if(caindo){
+            setImage(pinguimCaindo);
+            turn(10);
+    }
     }
     
     public void desaparece(){
