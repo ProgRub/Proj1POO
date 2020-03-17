d52c56d1282d768224aba973bc40b697b60175c4
ProgRub <ruben2000jgr@gmail.com>
2020-03-17 21:51:38 +0000

    Relampago nao precisava ser subclasse de target

--- Relâmpago.java
+++ Relâmpago.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Relâmpago extends Target
+public class Relâmpago extends Jogos
 {
     private GreenfootImage[] relampago;
     private int indice=0;
